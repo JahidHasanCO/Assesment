e33578dbc319a81010cc3b78d7ec4029d24d148e
jahidhasanco <vdjsovaj@gmail.com>
2022-10-30 17:53:36 +0600

    update

--- app/src/main/java/dev/jahidhasanco/assesment/presentation/activity/MainActivity.kt
+++ app/src/main/java/dev/jahidhasanco/assesment/presentation/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
@@ -14,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import dev.jahidhasanco.assesment.R
 import dev.jahidhasanco.assesment.data.model.User
 import dev.jahidhasanco.assesment.databinding.ActivityMainBinding
+import dev.jahidhasanco.assesment.databinding.UserItemDialogBinding
 import dev.jahidhasanco.assesment.presentation.adapter.OnUserClickListener
 import dev.jahidhasanco.assesment.presentation.adapter.UserAdapter
 import dev.jahidhasanco.assesment.presentation.viewmodel.StorageViewModel
@@ -31,12 +33,12 @@ class MainActivity : AppCompatActivity(), OnUserClickListener {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         storageViewModel.getUser()
+        userAdapter.setOnItemClickListener(this)
 
         lifecycle.coroutineScope.launchWhenCreated {
             storageViewModel.getUserDataStatus.collect {
                 if (it.isLoading) {
                     binding.progressBar.visibility = View.VISIBLE
-                    binding.userRecView.visibility = View.GONE
                 }
                 if (it.error.isNotBlank()) {
                     binding.progressBar.visibility = View.GONE
@@ -44,7 +46,6 @@ class MainActivity : AppCompatActivity(), OnUserClickListener {
                 }
                 it.data?.let { data ->
                     binding.progressBar.visibility = View.GONE
-                    binding.userRecView.visibility = View.VISIBLE
                     userList.clear()
                     userList.addAll(data)
                     userAdapter.setUserList(userList.toMutableList())
@@ -54,11 +55,16 @@ class MainActivity : AppCompatActivity(), OnUserClickListener {
 
         lifecycle.coroutineScope.launchWhenCreated {
             storageViewModel.deleteUserStatus.collect {
+                if (it.isLoading) {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
                 if (it.error.isNotBlank()) {
                     Toast.makeText(this@MainActivity, it.error, Toast.LENGTH_SHORT).show()
                 }
                 it.data?.let { data ->
                     Toast.makeText(this@MainActivity, data, Toast.LENGTH_SHORT).show()
+                    binding.progressBar.visibility = View.GONE
+                    storageViewModel.getUser()
                 }
             }
         }
@@ -77,7 +83,28 @@ class MainActivity : AppCompatActivity(), OnUserClickListener {
     }
 
     override fun onUserItemClick(user: User) {
-
+        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog)
+            .create()
+        val binding: UserItemDialogBinding = UserItemDialogBinding.inflate(layoutInflater)
+        val button = binding.closeDialog
+        binding.apply {
+            name.text = user.name
+            address.text = "Country: ${user.country}, City: ${user.city}"
+            var skillsWithComma = ""
+            user.skill.forEach {
+                skillsWithComma += "$it, "
+            }
+            skillsWithComma.dropLast(2)
+            skills.text = skillsWithComma
+            dob.text = user.dateOfBirth
+            resume.text = user.resumeTitle
+        }
+        builder.setView(binding.root)
+        button.setOnClickListener {
+            builder.dismiss()
+        }
+        builder.setCanceledOnTouchOutside(false)
+        builder.show()
     }
 
     override fun onItemDeleteClick(user: User) {
